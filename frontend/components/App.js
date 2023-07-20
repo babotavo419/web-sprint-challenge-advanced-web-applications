43391ebdeebc07214336aa85681ca620be63967f
babotavo419 <one.faith419@gmail.com>
2023-07-20 08:58:30 -0400

    ok

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -122,6 +122,7 @@ const deleteArticle = async (article_id, articleTitle) => {
 
   try {
     await axiosWithAuth.delete(`/articles/${article_id}`);
+    setArticles((prevArticles) => prevArticles.filter((article) => article.article_id !== article_id));
     setMessage(`Article ${articleTitle} was deleted, ${username}!`);
   } catch (error) {
     setMessage('Error deleting article.');
