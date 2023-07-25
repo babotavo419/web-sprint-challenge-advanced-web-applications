1980b0dc9cc76aa7de4df20fd84d9b7e3f1c99ed
babotavo419 <one.faith419@gmail.com>
2023-07-24 21:10:48 -0400

    ok

--- backend/helpers.js
+++ backend/helpers.js
@@ -94,17 +94,9 @@ async function login(user) {
   }
 }
 
-async function checkToken(tokenWithBearer) {
-  const token = tokenWithBearer.split(" ")[1];
-  try {
-    const decodedToken = jwt.verify(token, thisShouldBeSecret);
-    return decodedToken;
-  } catch (err) {
-    console.error("Error verifying token:", err);
-    throw err;
-  }
+async function checkToken(token) {
+  return jwt.verify(token, thisShouldBeSecret)
 }
-
 
 async function getArticles(token) {
   try {
