65f351c6ed96d34a779816c63039e366622088d8
babotavo419 <one.faith419@gmail.com>
2023-07-24 21:54:15 -0400

    ok

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -198,7 +198,7 @@ describe('Advanced Applications', () => {
     })
   })
   describe('Deleting an existing article', () => {
-    jest.setTimeout(1000);
+    jest.setTimeout(2000);
     test(`[9] Clicking delete button on an article
         - removes it from the page
         - a success message renders on the page
